6b0c8779721af03cdbd589287933097e2c7edfd7
HP <HP@DESKTOP-M1GORDV>
2025-06-09 00:26:16 -0500

    Clases creadas

---
+++ src/main/java/com/mycompany/asesorame/demo/AsesoraMeDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asesorame.demo;
+
+/**
+ *
+ * @author jeanc
+ */
+public class AsesoraMeDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
